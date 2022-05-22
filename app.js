20fffc988ade07c6329aa16a3f7b7607a36088e9
vrvijayshanker <vijayshankervr@gmail.com>
2022-05-22 21:39:06 +0530

    app.listen PORT changed

--- app.js
+++ app.js
@@ -69,7 +69,13 @@ app.get('/',function(req,res){
 
 
 
-app.listen(5000,()=>{
+app.listen(process.env.PORT || 5000,()=>{
     //Part #1, No.5: "server ready on 3000" --> "server ready on 5000"
     console.log("Server Ready on 5000");
 });
+
+//OLD CODE
+// app.listen(5000,()=>{
+//     //Part #1, No.5: "server ready on 3000" --> "server ready on 5000"
+//     console.log("Server Ready on 5000");
+// });
